73a70a977ba7c62540bcbaa948337a3bec8cf034
Andrew Evans <Andrew.Evans@babycenter.com>
2016-06-15 17:17:11 -0700

    Remove API call for members since it is being loaded into store via createRecord().

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,21 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   mockData: Ember.on('init', function() {
 
-    var self = this;
-    Ember.$.getJSON('/api/v1/members/').then(function(json) {
-      var data = json.data;
-      for (var i = 0; i < data.length; i++) {
-        self.store.push({
-          data: {
-            id: data[i].id,
-            type: 'member',
-            attributes: {
-              username: data[i].attributes.username,
-            }
-          },
-        });
-      }
-    });
     this.store.push({
       data: [
         {
