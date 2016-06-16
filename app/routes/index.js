387013a9edd731bfaa66a6ea291d06ccadb16dc0
Andrew Evans <Andrew.Evans@babycenter.com>
2016-06-16 11:51:30 -0700

    Revert "wip"
    This reverts commit b3cd8dd90364890d9f035e4dfd79909d1b7f40b6.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,21 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   mockData: Ember.on('init', function() {
 
+    var self = this;
+    Ember.$.getJSON('/api/v1/members/').then(function(json) {
+      var data = json.data;
+      for (var i = 0; i < data.length; i++) {
+        self.store.push({
+          data: {
+            id: data[i].id,
+            type: 'member',
+            attributes: {
+              username: data[i].attributes.username,
+            }
+          },
+        });
+      }
+    });
     this.store.push({
       data: [
         {
@@ -68,8 +83,6 @@ export default Ember.Route.extend({
     });
   }),
   model() {
-    window.console.log("in routes/index");
-
     return Ember.RSVP.hash({
       member: this.store.peekRecord('member', 16),
       chapter: this.store.peekRecord('chapter', 102),
