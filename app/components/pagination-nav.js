88a3a1fae25b877c10a7e0409aecddf03f0ccbdb
Andrew Evans <Andrew.Evans@babycenter.com>
2016-06-18 14:58:59 -0700

    Save all tags on back/next.
    * This is necessary at this point because input values shouldn't send when the input field is clicked.
    * Needs refactor since it shouldn't send all tags all the time, just the recent questions' tags.
    * The click action on questions shouldn't send the tag over the wire, but it should still add it to the data store.

--- app/components/pagination-nav.js
+++ app/components/pagination-nav.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    saveTags() {
+      window.console.log("In pagination-nav saveTags()");
+      this.sendAction('saveTags'); // Trigger the route's route-action
+      return true; // Bubble saveTags action up to hit index/chapters route
+    }
+  }
 });
