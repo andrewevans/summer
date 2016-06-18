e48bb5419bf97b373b323fcb95cfb3dce6428bd9
Andrew Evans <Andrew.Evans@babycenter.com>
2016-06-18 13:45:22 -0700

    Create generic equality helper.
    Handlebars doesn't have an equality helper out of the box. A switch statement would have been better for the purpose of choosing which template to use for form fields, but blocks for the case statements aren't allowed with helpers.

---
+++ tests/unit/helpers/if-eq-test.js
@@ -0,0 +1,10 @@
+import { ifEq } from 'summer/helpers/if-eq';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | if eq');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = ifEq([42]);
+  assert.ok(result);
+});
