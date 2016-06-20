b0a7147efa546817eaac4d4c7d443e7b040c7b2f
Andrew Evans <Andrew.Evans@babycenter.com>
2016-06-20 16:23:43 -0700

    Refactor: Move calculation of consequences into member-consequences service.
    This is still recalculating on every transition. This is because each question transition needs to check if it should terminate, and the results route checks which links to show.

--- app/routes/index/results.js
+++ app/routes/index/results.js
@@ -2,90 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   afterModel(model) {
-    var member = model.member,
+    var memberConsequences = this.get('member-consequences'),
+      member = model.member,
       chapter = model.chapter,
-      tags = member.get('tags'),
-      forwardToResults = false,
-      consequences = [], //@TODO: Consequences shouldn't recalculate on every transition
-      consequence_links = this.modelFor('index').consequence_links,
-      link;
+      consequence_links = this.modelFor('index').consequence_links;
 
-    var self = this;
-
-    tags.forEach(function(tag) {
-      var answer = tag.get('answer') || [],
-        questionId = parseInt(tag.get('questionId'));
-
-      //@TODO: This is business logic, doesn't belong here
-      switch (questionId) {
-
-        // Q: sex
-        case 1015:
-          if (answer.contains('male')) {
-            forwardToResults = true;
-          }
-          break;
-
-        // Q: age
-        case 1010:
-          if (answer.contains('13-')) {
-            forwardToResults = true;
-          }
-          break;
-
-        // Q: preg?
-        case 1004:
-          if (answer.contains('none')) {
-            forwardToResults = true;
-          }
-          break;
-
-        // Q: live in US?
-        case 1011:
-          if (answer.contains('no')) {
-            forwardToResults = true;
-          }
-          break;
-
-        case 1115:
-          if (answer.contains('condition-B')) {
-            link = consequence_links.objectAt(5);
-
-            if (! consequences.contains(link)) {
-              consequences.pushObject(link);
-            }
-          }
-
-          if (answer.contains('condition-C')) {
-            link = consequence_links.objectAt(6);
-
-            if (! consequences.contains(link)) {
-              consequences.pushObject(link);
-            }
-          }
-          break;
-
-        // Q: twins?
-        case 1116:
-          if (answer.contains('yes')) {
-            link = consequence_links.objectAt(8);
-
-            if (! consequences.contains(link)) {
-              consequences.pushObject(link);
-            }
-          }
-          break;
-
-        default:
-          break;
-      }
-
-      member.set('consequences', consequences);
-    });
-
-    if (forwardToResults) {
-      self.transitionTo('index.results', chapter.id);
-    }
+    memberConsequences.calculate(member, chapter, consequence_links, this);
   },
   model() {
     return Ember.RSVP.hash({
--- app/services/member-consequences.js
+++ app/services/member-consequences.js
@@ -1,4 +1,87 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  calculate(member, chapter, consequence_links, route) {
+      var tags = member.get('tags'),
+      forwardToResults = false,
+      consequences = [], //@TODO: Consequences shouldn't recalculate on every transition
+      link;
+
+    tags.forEach(function(tag) {
+      var answer = tag.get('answer') || [],
+        questionId = parseInt(tag.get('questionId'));
+
+      //@TODO: This is business logic, doesn't belong here
+      switch (questionId) {
+
+        // Q: sex
+        case 1015:
+          if (answer.contains('male')) {
+            forwardToResults = true;
+          }
+          break;
+
+        // Q: age
+        case 1010:
+          if (answer.contains('13-')) {
+            forwardToResults = true;
+          }
+          break;
+
+        // Q: preg?
+        case 1004:
+          if (answer.contains('none')) {
+            forwardToResults = true;
+          }
+          break;
+
+        // Q: live in US?
+        case 1011:
+          if (answer.contains('no')) {
+            forwardToResults = true;
+          }
+          break;
+
+        case 1115:
+          if (answer.contains('condition-B')) {
+            link = consequence_links.objectAt(5);
+
+            if (! consequences.contains(link)) {
+              consequences.pushObject(link);
+            }
+          }
+
+          if (answer.contains('condition-C')) {
+            link = consequence_links.objectAt(6);
+
+            if (! consequences.contains(link)) {
+              consequences.pushObject(link);
+            }
+          }
+          break;
+
+        // Q: twins?
+        case 1116:
+          if (answer.contains('yes')) {
+            link = consequence_links.objectAt(8);
+
+            if (! consequences.contains(link)) {
+              consequences.pushObject(link);
+            }
+          }
+          break;
+
+        default:
+          break;
+      }
+
+      member.set('consequences', consequences);
+    });
+
+    if (forwardToResults) {
+      route.transitionTo('index.results', chapter.id);
+    }
+
+    return;
+  }
 });
