ebf3d0f47e04eed085fcc84faa6f70543b5d2f3d
Andrew Evans <Andrew.Evans@babycenter.com>
2016-06-21 12:51:37 -0700

    Refactor: Move welcome route into chapters.
    It should be within chapters because it pertains to chapter-specific data. If needed later, there can be a chapters list page outside of chapters.

--- app/routes/index/welcome.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model() {
-    return Ember.RSVP.hash({
-      chapters: this.modelFor('index').chapters,
-    });
-  }
-});
--- tests/unit/routes/index/chapter/welcome-test.js
+++ tests/unit/routes/index/chapter/welcome-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:index/welcome', 'Unit | Route | index/welcome', {
+moduleFor('route:index/chapter/welcome', 'Unit | Route | index/chapter/welcome', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
