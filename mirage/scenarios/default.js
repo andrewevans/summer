f6ee3f46ef0aefa35dac74f7c955004940a01e04
Andrew Evans <Andrew.Evans@babycenter.com>
2016-07-02 22:53:22 -0700

    Question about height is select-dropdown in inches. Needs refactor.
    This may change to something more user-friendly, such as two input text fields for feet/inches.

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -270,23 +270,17 @@ export default function(server) {
   question = chapter.createQuestion({
     title: "Hhow tall are you?",
     description: "",
-    type: "select-multi",
+    type: "select-dropdown",
   });
 
-  question.createOption({
-    value: "average",
-    text: "Average",
-  });
+  // Populate each week's option
+  for (let feet = 3; feet <= 7; feet++) {
 
-  question.createOption({
-    value: "below-average",
-    text: "Below Average",
-  });
+    for (let inches = 0; inches <= 11; inches++) {
+      server.create('option', { value: ((feet * 12) + inches), text: feet +'ft ' + inches + ' in', question: question });
+    }
+  }
 
-  question.createOption({
-    value: "above-average",
-    text: "Above average",
-  });
   // End question + its options
 
   // Create a question + its options
