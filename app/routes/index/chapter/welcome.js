9aa8629736b4b05165b680ebd84555f5b1ab7565
Andrew Evans <Andrew.Evans@babycenter.com>
2016-07-05 11:51:05 -0700

    Fix: Reset pagination on welcome page.
    This causes the pagination to hide correctly.

--- app/routes/index/chapter/welcome.js
+++ app/routes/index/chapter/welcome.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel() {
+
+    // Reset pagination
+    this.modelFor('index/chapter').chapter
+      .set('pagination', null);
+  }
 });
