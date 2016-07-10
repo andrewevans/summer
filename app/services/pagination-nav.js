ec6dd6583a13fa02c54db7976b7939202039f2a3
andrewe <andrewevansc@gmail.com>
2016-07-10 16:19:28 -0700

    Create service to update pagination.

---
+++ app/services/pagination-nav.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  /**
+   * Pagination logic to update current state
+   * @param chapter
+   * @param progress
+   */
+  update(chapter, progress, sequence_num) {
+    progress.sequence_num = sequence_num; // Update member's current progress for this chapter
+
+    var next = progress.sequence_num + 1,
+      prev = progress.sequence_num - 1,
+      total = chapter.get('questionsLength');
+
+    next = (next > total ? false : next);
+    prev = (prev < 1 ? false : prev);
+
+    chapter.set('pagination', {
+      'sequence_num': progress.sequence_num,
+      'next': next,
+      'prev': prev,
+      'total': total,
+      'percentageComplete': Math.floor(((progress.sequence_num - 1)/total) * 100),
+    });
+  }
+});
