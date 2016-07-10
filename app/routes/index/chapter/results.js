93d50f04aeae5406cf4a26f3a4852a343c0be465
andrewe <andrewevansc@gmail.com>
2016-07-10 15:41:35 -0700

    Results page updates sequence number to 0.
    This hides the pagination nav.

--- app/routes/index/chapter/results.js
+++ app/routes/index/chapter/results.js
@@ -10,9 +10,40 @@ export default Ember.Route.extend({
     memberConsequences.calculate(member, chapter, consequence_links, this);
   },
   model() {
-    return Ember.RSVP.hash({
+    var chapter = this.modelFor('index/chapter').chapter,
+      member = this.modelFor('index').member,
+      progresses = member.get('progresses');
+
+    progresses.forEach(progress => {
+      if (progress.chapter_id === parseInt(chapter.id)) {
+        progress.sequence_num = null; // Update member's current progress for this chapter
+
+        /**
+         * Pagination logic
+         */
+        var next = progress.sequence_num + 1,
+          prev = progress.sequence_num - 1,
+          total = chapter.get('questionsLength');
+
+        next = (next > total ? false : next);
+        prev = (prev < 1 ? false : prev);
+
+        chapter.set('pagination', {
+          'sequence_num': progress.sequence_num,
+          'next': next,
+          'prev': prev,
+          'total': total,
+          'percentageComplete': Math.floor(((progress.sequence_num - 1) / total) * 100),
+        });
+        /**
+         * End pagination logic
+         */
+      }
+    });
+
+      return Ember.RSVP.hash({
       member: this.modelFor('index').member,
-      chapter: this.modelFor('index/chapter').chapter,
+      chapter: chapter,
     });
   }
 });
