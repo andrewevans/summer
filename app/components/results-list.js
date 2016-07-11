5d044f5af1a19b0ba695243a308681b2b1ce9c57
andrewe <andrewevansc@gmail.com>
2016-07-10 23:35:13 -0700

    Remove console log.

--- app/components/results-list.js
+++ app/components/results-list.js
@@ -7,7 +7,7 @@ export default Ember.Component.extend({
       this.toggleProperty('isShowingResults');
     },
     saveTags() {
-      window.console.log("In results-list saveTags()");
+      Ember.Logger.log("In results-list saveTags()");
       this.sendAction('saveTags');
       return true; // Bubble saveTags action up to hit index/chapters route
     }
