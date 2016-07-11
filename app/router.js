a00911f8cab1d44724efe494fbd4d9989d50eefe
andrewe <andrewevansc@gmail.com>
2016-07-10 23:00:43 -0700

    Remove chapters (not chapter) route.

--- app/router.js
+++ app/router.js
@@ -7,7 +7,6 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('people');
-  this.route('chapters');
   this.route('index', { path: '' }, function() {
     this.route('chapter', { path: '/chapters/:id' }, function() {
       this.route('question', { path: '/question/:sequence_num' });
