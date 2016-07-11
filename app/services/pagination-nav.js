94e7e7f5d9990b215cf32d823a0c8ed58c6a4f25
Andrew Evans <Andrew.Evans@babycenter.com>
2016-07-11 12:27:36 -0700

    Refactor: In pagination nav service, filter progresses for only the progress for this chapter, and update only that one's sequence number, and update pagination according to that one's sequence number thereafter.
    This is still checking if there is a chapter_progress, but that should be guaranteed at this point because the chapter sets it if it does not exist. Needs refactor, but it is left in for now as to illustrate the similarity to the chapter route's evaluating and/or updating of the sequence number.

--- app/services/pagination-nav.js
+++ app/services/pagination-nav.js
@@ -6,28 +6,23 @@ export default Ember.Service.extend({
    */
   update(member, chapter, sequence_num) {
 
-    var progresses = member.get('progresses');
+    var progresses = member.get('progresses'),
+      chapter_progress = progresses.filterBy('chapter_id', parseInt(chapter.id)).objectAt(0); // Get first matching progress
 
-    // Loop through member's progresses until this chapter's progress is found
-    progresses.forEach(progress => {
-      if (progress.chapter_id === parseInt(chapter.id)) {
-        progress.sequence_num = sequence_num; // Update member's current progress for this chapter
-
-        var next = progress.sequence_num + 1,
-          prev = progress.sequence_num - 1,
-          total = chapter.get('questionsLength');
-
-        next = (next > total ? false : next);
-        prev = (prev < 1 ? false : prev);
-
-        member.set('pagination', {
-          'sequence_num': progress.sequence_num,
-          'next': next,
-          'prev': prev,
-          'total': total,
-          'percentageComplete': Math.floor(((progress.sequence_num - 1)/total) * 100),
-        });
-      }
-    });
+    if (chapter_progress) {
+      chapter_progress.sequence_num = sequence_num; // Update member's current progress for this chapter
+      var next = chapter_progress.sequence_num + 1,
+        prev = chapter_progress.sequence_num - 1,
+        total = chapter.get('questionsLength');
+      next = (next > total ? false : next);
+      prev = (prev < 1 ? false : prev);
+      member.set('pagination', {
+        'sequence_num': chapter_progress.sequence_num,
+        'next': next,
+        'prev': prev,
+        'total': total,
+        'percentageComplete': Math.floor(((chapter_progress.sequence_num - 1) / total) * 100),
+      });
+    }
   }
 });
