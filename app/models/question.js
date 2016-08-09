43c335867b92c7ccd120f6b3420e5e754b67d2c8
Andrew Evans <Andrew.Evans@babycenter.com>
2016-08-08 17:00:56 -0700

    CS.

--- app/models/question.js
+++ app/models/question.js
@@ -8,6 +8,6 @@ export default Model.extend({
   type: attr(),
   options: hasMany('option'),
   chapter: belongsTo('chapter'),
-  questions: hasMany('question', { inverse: 'question' }),
-  question: belongsTo('question', { inverse: 'questions' }),
+  questions: hasMany('question', { inverse: 'question' }), // Relationship to its child questions
+  question: belongsTo('question', { inverse: 'questions' }), // Relationship to its parent question
 });
