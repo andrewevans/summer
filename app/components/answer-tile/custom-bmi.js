9e8df4751aae3ea346adb30e1da1a76027157f58
Andrew Evans <Andrew.Evans@babycenter.com>
2016-08-09 14:50:10 -0700

    Create custom component to handle weight with hidden child question for BMI, but without updating the BMI tag.

---
+++ app/components/answer-tile/custom-bmi.js
@@ -0,0 +1,63 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  child_question: null,
+  child_question_tag: null,
+  didReceiveAttrs() {
+    this.updateInputValue();
+  },
+  // updateInputValue() and component/answer-tile's updateSelecteds()
+  // need to be maintained together, because they do the same task but for different inputs.
+  updateInputValue() {
+    var member = this.get('member'),
+      chapter = this.get('chapter'),
+      parent_question = this.get('question'),
+
+    // The parent question must have a child question to use the custom-extra component
+      child_question = parent_question.get('questions').objectAt(0);
+
+    // Find the tag corresponding to the child question
+    let tags = member.get('tags')
+      .filterBy('chapterId', parseInt(chapter.id))
+      .filterBy('questionId', parseInt(child_question.id)),
+      tag;
+
+    // Find the corresponding tag, or create it if it doesn't exist
+    if (tags.get('length')) {
+
+      // The tag exists, so use that one
+      tag = tags.objectAt(0);
+    } else {
+
+      // The tag does not exist yet, so create it
+      tag = this.get('store').createRecord('tag', {
+        member: member,
+        chapterId: parseInt(chapter.id),
+        questionId: parseInt(child_question.id),
+        answer: [],
+      });
+    }
+
+    // Set the properties to be used in the template
+    this.set('child_question_tag', tag);
+    this.set('child_question', child_question);
+  },
+  saveTagWithSpecialCases: function(member, chapter, question, option, tag) {
+    var answer = tag.get('answer') || [];
+
+    option.set('value', answer);
+
+    // Calculate BMI
+
+    // Save BMI with updateTagCustom with child tag
+  },
+  actions: {
+    updateTag(member, chapter, question, option, tag) {
+      // Save weight
+      this.sendAction('updateTag', option, tag);
+
+      this.saveTagWithSpecialCases(member, chapter, question, option, tag);
+    },
+  },
+});
