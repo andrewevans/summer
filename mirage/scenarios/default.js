b387204fe733fb548545f84abd1c6fa099f5cd9b
Andrew Evans <Andrew.Evans@babycenter.com>
2016-08-21 15:45:22 -0700

    Mock data. CS.

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -4,7 +4,6 @@ export default function(server) {
 
   let member = server.schema.members.find(4); // Get member "lana"
 
-
   server.create('response', {
     memberId: 4,
     surveyId: 102,
@@ -39,7 +38,7 @@ export default function(server) {
   question.createOption({
     value: "yes",
     text: "Yes",
-  })
+  });
 
   question.createOption({
     value: "no",
@@ -57,7 +56,7 @@ export default function(server) {
   question.createOption({
     value: "yes",
     text: "Yes",
-  })
+  });
 
   question.createOption({
     value: "no",
@@ -68,7 +67,6 @@ export default function(server) {
   // Create a question + its options
   question = chapter.createQuestion({
     title: "How old are you?",
-    description: "",
     type: "select",
     slug: "age",
   });
@@ -98,7 +96,6 @@ export default function(server) {
     text: "45 or older",
   });
   // End question + its options
-
 
   // Create a question + its options
   question = chapter.createQuestion({
@@ -142,7 +139,7 @@ export default function(server) {
   question.createOption({
     value: "yes",
     text: "Yes",
-  })
+  });
 
   question.createOption({
     value: "no",
@@ -259,7 +256,6 @@ export default function(server) {
   // Create a question + its options
   var question_prev_condition_extra = chapter.createQuestion({
     title: "Other conditions (Please specify.)",
-    description: "",
     type: "hidden",
     question: question_prev_condition,
   });
@@ -322,7 +318,8 @@ export default function(server) {
 
   // Create a question + its options
   let question_conditions = chapter.createQuestion({
-    title: "Before this pregnancy, or now that you're pregnant, were you diagnosed with any of the following conditions? (Please select all that apply.)",
+    title: "Before this pregnancy, or now that you're pregnant, were you diagnosed with any of the following conditions?" +
+    " (Please select all that apply.)",
     type: "custom-extra",
     slug: "current-conditions"
   });
@@ -367,11 +364,9 @@ export default function(server) {
     text: "None of the above",
   });
 
-
   // Create a question + its options
   var question_conditions_extra = chapter.createQuestion({
     title: "Other conditions (Please specify.)",
-    description: "",
     type: "hidden",
     question: question_conditions,
   });
@@ -384,7 +379,8 @@ export default function(server) {
 
   // Create a question + its options
   var question_problems = chapter.createQuestion({
-    title: "In this pregnancy, have you experienced any of these health problems? (Please select all that apply.) (filter based on pregnancy stage)",
+    title: "In this pregnancy, have you experienced any of these health problems? (Please select all that apply.) " +
+    "(filter based on pregnancy stage)",
     type: "custom-extra",
     slug: "problems",
   });
@@ -415,7 +411,6 @@ export default function(server) {
   // Create a question + its options
   var question_problems_extra = chapter.createQuestion({
     title: "Other problems (Please specify.)",
-    description: "",
     type: "hidden",
     question: question_problems,
   });
@@ -426,163 +421,46 @@ export default function(server) {
   });
   // End question + its options
 
-  // Create a custom question + its options
-  var question_condition = chapter.createQuestion({
-    title: "Previous or current conditions",
-    description: "",
-    type: "custom-extra",
-  });
-
-  question_condition.createOption({
-    value: "condition-01",
-    text: "Condition 01",
-  });
-
-  question_condition.createOption({
-    value: "condition-02",
-    text: "Condition 02",
-  });
-
-  question_condition.createOption({
-    value: "condition-03",
-    text: "Condition 03",
-  });
-
-  question_condition.createOption({
-    value: "condition-04",
-    text: "Condition 04",
-  });
-
-  question_condition.createOption({
-    value: "condition-05",
-    text: "Condition 05",
-  });
-
-  question_condition.createOption({
-    value: "condition-A",
-    text: "Condition A",
-  });
-
-  question_condition.createOption({
-    value: "condition-B",
-    text: "Condition B",
-  });
-
-  question_condition.createOption({
-    value: "condition-C",
-    text: "Condition C",
-  });
-
-  question_condition.createOption({
-    value: "condition-D",
-    text: "Condition D",
-  });
-
-  question_condition.createOption({
-    value: "condition-E",
-    text: "Condition E",
-  });
-
-  question_condition.createOption({
-    value: "condition-F",
-    text: "Condition F",
-  });
-
-  question_condition.createOption({
-    value: "condition-G",
-    text: "Condition G",
-  });
-
-  question_condition.createOption({
-    value: "__none-true",
-    text: "None of the above",
-  });
-
-  question_condition.createOption({
-    value: "__none-neutral",
-    text: "I prefer not to answer",
-  });
-  // End question + its options
-
-  // Create a question + its options
-  var question_condition_extra = chapter.createQuestion({
-    title: "Any other previous or current conditions?",
-    description: "",
-    type: "hidden",
-    question: question_condition,
-  });
-
-  question_condition_extra.createOption({
-    value: "",
-    text: "__input-extra",
-  });
-  // End question + its options
-
   // Create a question + its options
   question = chapter.createQuestion({
-    title: "Now that you're pregnant, do you smoke cigarettes?",
-    description: "",
+    title: "Are you taking any medications to prevent preterm birth?",
     type: "select",
-  });
-
-  question.createOption({
-    value: "no",
-    text: "No",
+    slug: "preterm-meds",
   });
 
   question.createOption({
     value: "yes",
     text: "Yes",
   });
-  // End question + its options
-
-  // Create a question + its options
-  question = chapter.createQuestion({
-    title: "Now that you're pregnant, how often do you have a drink that contains alcohol?",
-    description: "",
-    type: "select",
-  });
 
   question.createOption({
     value: "no",
     text: "No",
   });
-
-  question.createOption({
-    value: "yes",
-    text: "Yes",
-  });
   // End question + its options
 
   // Create a question + its options
   question = chapter.createQuestion({
-    title: "Now that you're pregnant, do you ever use recreational drugs or misuse prescription medication, such as pain drugs?",
-    description: "",
+    title: "Earlier in this survey, you indicated that you experienced the following medical conditions or risk factors. " +
+    "Please check the conditions listed below for which you are currently receiving care from a health provider.",
     type: "select-multi",
   });
 
-  question.createOption({
-    value: "heroin",
-    text: "Heroin",
-  });
+  //@TODO: In app, only list options that exist in the preg-conditions question
 
-  question.createOption({
-    value: "cocaine",
-    text: "Cocaine",
-  });
-
-  question.createOption({
-    value: "marijuana",
-    text: "Marijuana",
-  });
+  for (let i = 0; i < preg_conditions.length; i++) {
+    question.createOption({
+      value: preg_conditions[i].dasherize(),
+      text: preg_conditions[i],
+    });
+  }
   // End question + its options
 
   // Create a question + its options
-  //@TODO: This question needs two input fields: feet, inches
   question = chapter.createQuestion({
     title: "How tall are you?",
-    description: "",
     type: "custom-height",
+    slug: "height",
   });
 
   question.createOption({
@@ -598,7 +476,7 @@ export default function(server) {
 
   // Create a question + its options
   var question_weight = chapter.createQuestion({
-    title: "How much did you weigh before you were pregnant?",
+    title: "How much did you weigh *before* you were pregnant?",
     description: "Be honest :)",
     type: "custom-weight",
   });
@@ -609,7 +487,6 @@ export default function(server) {
 
   var question_bmi = chapter.createQuestion({
     title: "What is your BMI?",
-    description: "",
     type: "hidden",
   });
 
@@ -617,10 +494,127 @@ export default function(server) {
     value: "",
     text: "__input-bmi",
   });
+  // End question + its options
 
+  // Create a question + its options
+  question = chapter.createQuestion({
+    title: "What is your race or ethnicity? (Please select all that apply.)",
+    type: "select-multi",
+  });
+
+  let ethnicity = [
+    'African American',
+    'Asian',
+    'Caribbean',
+    'Caucasian',
+    'Latina',
+    'Jewish',
+    'Native American',
+    'Pacific Islander',
+  ];
+
+  for (let i = 0; i < ethnicity.length; i++) {
+    question.createOption({
+      value: ethnicity[i].dasherize(),
+      text: ethnicity[i],
+    });
+  }
+
+  question.createOption({
+    value: "__none-neutral",
+    text: "I prefer not to answer",
+  });
+  // End question + its options
+
+  // Create a question + its options
+  question = chapter.createQuestion({
+    title: "Now that you're pregnant, do you smoke cigarettes?",
+    type: "select",
+    slug: "smoking",
+  });
+
+  let smoking = [
+    'Yes, but I\'m thinking about quitting.',
+    'Yes, and I don\'t plan to quit.',
+    'No, I quit smoking when I started trying to conceive.',
+    'No, I quit smoking when I found I out I was pregnant.',
+    'No, I quit smoking a few weeks into my pregnancy.',
+    'No, I\'ve never smoked cigarettes.',
+  ];
+
+  for (let i = 0; i < smoking.length; i++) {
+    question.createOption({
+      value: smoking[i].dasherize(),
+      text: smoking[i],
+    });
+  }
+
+  question.createOption({
+    value: "__none-neutral",
+    text: "I prefer not to answer",
+  });
+  // End question + its options
+
+  // Create a question + its options
+  question = chapter.createQuestion({
+    title: "Now that you're pregnant, how often do you have a drink that contains alcohol?",
+    type: "select",
+  });
+
+  let alcohol = [
+    'Nearly every day',
+    'Three or four days a week',
+    'Two days a week',
+    'Once a month',
+    'Less than once a month',
+    'I don\'t drink alcohol.',
+  ];
+
+  for (let i = 0; i < alcohol.length; i++) {
+    question.createOption({
+      value: alcohol[i].dasherize(),
+      text: alcohol[i],
+    });
+  }
+  // End question + its options
+
+  // Create a question + its options
+  question = chapter.createQuestion({
+    title: "Now that you are pregnant, do you ever use recreational drugs or misuse prescription medication, such as " +
+    "pain drugs? (Please select all that apply.)",
+    type: "select-multi",
+    slug: "drugs",
+  });
+
+  let drugs = [
+    'Cocaine',
+    'Heroin',
+    'Marijuana',
+    'Opioid pain medication, such as fentanyl, acetaminophen/hydrocodone, or oxycodone',
+    'Other recreational drugs',
+  ];
+
+  for (let i = 0; i < drugs.length; i++) {
+    question.createOption({
+      value: drugs[i].dasherize(),
+      text: drugs[i],
+    });
+  }
+
+  question.createOption({
+    value: "__none-true",
+    text: "None of the above",
+  });
+
+  question.createOption({
+    value: "__none-neutral",
+    text: "I prefer not to answer",
+  });
+  // End question + its options
+
+  // Create a question + its options
   var question_preg35 = chapter.createQuestion({
     title: "Are you 35 or older and this is your first pregnancy?",
-    description: "",
     type: "hidden",
     slug: "preg-35",
   });
@@ -629,26 +623,16 @@ export default function(server) {
     value: "",
     text: "",
   });
+  // End question + its options
 
   // Create a question + its options
   question = chapter.createQuestion({
-    title: "What is your ethnicity and/or ancestry?",
-    type: "select-multi",
+    title: "Are you worried about anything concerning your pregnancy or the health of your baby?",
+    type: "input",
   });
 
   question.createOption({
-    value: "white",
-    text: "White",
-  });
-
-  question.createOption({
-    value: "native-american",
-    text: "Native American",
-  });
-
-  question.createOption({
-    value: "asian",
-    text: "Asian",
+    value: "", // Blank because it is a text input field
   });
   // End question + its options
 
