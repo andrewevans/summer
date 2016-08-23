99c85888d51293171b9fcbbe4401f9362c218b52
Andrew Evans <Andrew.Evans@babycenter.com>
2016-08-23 14:50:34 -0700

    Allow the server's creating and updating response of response code 201 with no response body.
    Overriding 'createRecord' handles the request and the response.

--- app/adapters/tag.js
+++ app/adapters/tag.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
 export default JSONAPIAdapter.extend({
@@ -15,5 +16,28 @@ export default JSONAPIAdapter.extend({
     }
 
     return this.namespace + '/responses';
+  },
+  createRecord(store, type, snapshot) {
+    var data = this.serialize(snapshot, { includeId: true }),
+      url = this.buildURL(type.modelName, null, snapshot, 'createRecord');
+
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      Ember.$.ajax({
+        type: 'POST',
+        url: url,
+        headers: {
+          'Content-Type':'application/json'
+        },
+        data: JSON.stringify(data)
+      }).then(function(data) {
+        Ember.run(null, resolve, data);
+      }, function(jqXHR) {
+        jqXHR.then = null; // tame jQuery's ill mannered promises
+        Ember.run(null, reject, jqXHR);
+      });
+    });
+  },
+  updateRecord(store, type, snapshot) {
+    return this.createRecord(store, type, snapshot);
   },
 });
